320e693b9ba4f3881972009d473eada3ef0884f3
Jinlee0206 <ljs4784@gmail.com>
2024-06-05 23:18:45 +0900

    20240605_Study

---
+++ Source/Study_240601/GameMode/DefaultGameMode.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DefaultGameMode.h"
+#include "../Player/PlayerCharacter.h"
+
+ADefaultGameMode::ADefaultGameMode()
+{
+	// 모든 언리얼 클래스들은 클래스이름::StaticClass() 함수를 이용해서 어떤 타입(자신의 클래스 정보)인지를 반환하게 만들어두었다.
+	DefaultPawnClass = APlayerCharacter::StaticClass();
+}
--- Source/Study_240601/Player/PlayerCharacter.h
+++ Source/Study_240601/Player/PlayerCharacter.h
@@ -2,7 +2,7 @@
 
 #pragma once
 
-#include "EngineMinimal.h"
+#include "../GameInfo.h"
 #include "GameFramework/Character.h"
 #include "PlayerCharacter.generated.h"
 
